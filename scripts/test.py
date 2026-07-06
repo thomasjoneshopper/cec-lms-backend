c691fc23acce5c0f3f874c6c998e524e178ae59e
thomasjoneshopper <thomasjoneshopper@gmail.com>
2026-07-06 17:57:19 -0500

    bug fixes

--- scripts/test.py
+++ scripts/test.py
@@ -1,3 +1,38 @@
+import requests
 from cec_lms_backend import db
 
-with open("../../cec-loto-lms/lessonData.json") as f: db.utils.load_content(f)
+DOMAIN = "http://127.0.0.1:5000"
+
+def print_response(r: requests.Response):
+    print(f"{r.url}")
+    print(f"    {r.status_code} {r.reason}")
+    for key, value in r.headers.items():
+        print(f"    {key}: {value}")
+    print()
+    for line in r.text.split("\n"):
+        print(f"    {line}")
+    print()
+
+def test_api():
+    with requests.sessions.session() as s:
+        # auth
+        login_creds = {
+            "employee_number": "0000",
+            "name": "test",
+        }
+        r = s.post(DOMAIN+"/auth/login", json=login_creds)
+        print_response(r)
+
+        r = s.get(DOMAIN+"/auth/me")
+        print_response(r)
+
+        # auth logout
+        r = s.post(DOMAIN+"/auth/logout")
+        print_response(r)
+
+def test_load_content():
+    with open("../../cec-loto-lms/lessonData.json") as f: 
+        db.utils.load_content(f)
+
+if __name__ == "__main__":
+    test_api()
